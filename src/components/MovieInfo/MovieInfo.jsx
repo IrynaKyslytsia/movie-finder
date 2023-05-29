347ed545a57ef9a9c4c08bbc0b7b091410f0059e
IrynaKyslytsia <i.kislica89@gmail.com>
2023-05-30 00:40:57 +0300

    Styled MovieInfo.jsx

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,22 +1,26 @@
+import { MovieCard, Poster, Info, Title, Text, Subtitle } from "./MovieInfo.styled";
+
 const MovieInfo = ({ movie }) => {
     const { genres, title, overview, vote_average, poster_path, release_date } = movie;
 
     return (
-        <>
-            <img
+        <MovieCard>
+            <Poster
                 src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
                 alt={title} />
-            <h2>{`${title} (${release_date.slice(0, 4)})`}</h2>
-            <p>User Score: {(vote_average * 10).toFixed(0)} %</p>
-            <h3>Overview</h3>
-            <p>{overview}</p>
-            <h3>Genres</h3>
-            <ul>
-                {genres.map(({ id, name }) => (
+            <Info>
+                <Title>{`${title} (${release_date.slice(0, 4)})`}</Title>
+                <Text><b>User Score: {(vote_average * 10).toFixed(0)} %</b></Text>
+                <Subtitle>Overview</Subtitle>
+                <Text>{overview}</Text>
+                <Subtitle>Genres</Subtitle>
+                <Text>
+                    {genres.map(({ id, name }) => (
                     <li key={id}>{name}</li>
-                ))}
-            </ul>
-        </>
+                    ))}
+                </Text>
+            </Info>
+        </MovieCard>
     )
 };
 
---
+++ src/components/MovieInfo/MovieInfo.styled.jsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+export const MovieCard = styled.div`
+    display: flex;
+    gap: 32px;
+    margin-top: 24px;
+    margin-bottom: 40px;
+`;
+
+export const Poster = styled.img`
+    width: 320px;
+`;
+
+export const Info = styled.div`
+    padding: 16px;
+`;
+
+export const Title = styled.h2`
+    font-size: 36px;
+    margin-bottom: 48px;
+`;
+
+export const Text = styled.p`
+    font-size: 20px;
+    margin-bottom: 24px;
+`;
+
+export const Subtitle = styled.h3`
+    font-size: 24px;
+    margin-bottom: 16px;
+`
