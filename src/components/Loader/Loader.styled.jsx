6996b678eb8e098a0941dc5043fb5db612084e0c
IrynaKyslytsia <114502274+IrynaKyslytsia@users.noreply.github.com>
2023-10-09 22:35:24 +0300

    Update Loader.styled.jsx

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 export const LoaderWrap = styled.div`
-    margin-left: auto;
-    margin-right: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 60px;
 `;
